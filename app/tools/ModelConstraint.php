7435fdffbc534d28b67a62f65fdc5dd189f8225f
Corentin Legros <corentin.legros@gmail.com>
2014-10-12 18:39:03 +0200

    ModelConstraint is now working well :)

--- app/tools/ModelConstraint.php
+++ app/tools/ModelConstraint.php
@@ -1,24 +1,30 @@
 <?php
 namespace App\Tools;
 
-use App\Models\AbstractModel;
-use Symfony\Component\Config\Definition\Exception\Exception;
-
+/**
+ * Class ModelConstraint.
+ * Provides methods to check data coherence through models
+ *
+ * @package App\Tools
+ */
 class ModelConstraint
 {
     /**
      * Allows to check a row with a specified value already exists in a column A
      * inside a table B in a database C
      *
-     *
+     * @param string $strClassModel
+     * @param string $strColumn
+     * @param mixed $mixedValue
      * @return boolean
+     * @throws \Exception
      */
     public static function alreadyColumnExists($strClassModel, $strColumn, $mixedValue)
     {
         self::checkMappingOptions(array('key_model' => $strClassModel, 'key_column' => $strColumn));
 
         return $strClassModel::getGenericList(
-            array($strClassModel::getModelInformation('columns')['primary_key']),
+            array($strClassModel::getModelInformation('primary_key')),
             array(
                 'where' => array(
                     $strColumn => array('clause' => '=', 'value' => $mixedValue),
@@ -31,12 +37,12 @@ class ModelConstraint
     /**
      * Allows to check a row with a specified already exists in a column A inside a table B in a database C
      *
-     * @param $strClassModel
-     * @param $strColumnA
-     * @param $mixedValueA
-     * @param $strColumnB
-     * @param $mixedValueB
-     * @return bool
+     * @param string $strClassModel
+     * @param string $strColumnA
+     * @param mixed $mixedValueA
+     * @param string $strColumnB
+     * @param mixed $mixedValueB
+     * @return boolean
      * @throws \Exception
      */
     public static function alreadyPairColumnExists($strClassModel, $strColumnA, $mixedValueA, $strColumnB, $mixedValueB)
@@ -45,7 +51,7 @@ class ModelConstraint
         self::checkMappingOptions(array('key_model' => $strClassModel, 'key_column' => $strColumnB));
 
         return $strClassModel::getGenericList(
-            array($strClassModel::getModelInformation('columns')['primary_key']),
+            array($strClassModel::getModelInformation('primary_key')),
             array(
                 'where' => array(
                     $strColumnA => array('clause' => '=', 'value' => $mixedValueA),
@@ -64,24 +70,20 @@ class ModelConstraint
     protected static function checkMappingOptions(array $hashOptions)
     {
         if (isset($hashOptions['key_model'])) {
-            $hashOptions['model'] = isset($hashOptions[$hashOptions['key_model']]) ? $hashOptions[$hashOptions['key_model']] : '';
+            $hashOptions['model'] = $hashOptions['key_model'];
         }
         if (isset($hashOptions['key_column'])) {
-            $hashOptions['column'] = isset($hashOptions[$hashOptions['key_column']]) ? $hashOptions[$hashOptions['key_column']] : '';
-        }
-        if (isset($hashOptions['key_value'])) {
-            $hashOptions['value'] = isset($hashOptions[$hashOptions['key_value']]) ? $hashOptions[$hashOptions['key_value']] : '';
+            $hashOptions['column'] = $hashOptions['key_column'];
         }
 
         $hashOptions['model'] = isset($hashOptions['model']) ? $hashOptions['model'] : '';
         $hashOptions['column'] = isset($hashOptions['column']) ? $hashOptions['column'] : '';
-        $hashOptions['value'] = isset($hashOptions['value']) ? $hashOptions['value'] : '';
 
         if (!class_exists($hashOptions['model'])) {
             throw new \Exception(sprintf('ModelConstraint - Non-existent model provided (%s)', $hashOptions['model']));
         }
 
-        if (!isset($hashOptions['model']::getModelInformation('columns')[$hashOptions['column']])) {
+        if (!isset($hashOptions['model']::getModelInformation('available_columns')[$hashOptions['column']])) {
             throw new \Exception(
                 sprintf(
                     'ModelConstraint - Non-existent column provided for model %s (%s)',
