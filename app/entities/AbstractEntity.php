f8fee512eeb87177c1e4c3563d3adb5b7fb6c8cc
Corentin Legros <corentin.legros@gmail.com>
2015-09-29 03:58:19 +0200

    Entities get normal behaviour

--- app/entities/AbstractEntity.php
+++ app/entities/AbstractEntity.php
@@ -1,19 +1,28 @@
 <?php
 namespace App\Entities;
 use App\Exceptions\EntityException;
+use Traversable;
 
 /**
  * Class AbstractEntity.
  * Should be a base for all entities that used models.
  *
+ * This layer provides entity as array to get an easier handling
+ *
  * @package App\Entities
  */
-abstract class AbstractEntity
+abstract class AbstractEntity implements \IteratorAggregate, \ArrayAccess
 {
     const PROCESS_CREATE = 0;
     const PROCESS_UPDATE = 1;
 
     protected $intId = 0;
+
+    /**
+     * Entity attributes
+     * @var array
+     */
+    protected $hashFields = array();
 
     /**
      * Construct a new entity that used models layer
@@ -61,4 +70,82 @@ abstract class AbstractEntity
         }
         return $this->processSave($hashData);
     }
-}
+
+     /**
+      * (PHP 5 &gt;= 5.0.0)<br/>
+      * Retrieve an external iterator
+      * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+      * @return Traversable An instance of an object implementing <b>Iterator</b> or
+      * <b>Traversable</b>
+      */
+     public function getIterator()
+     {
+         return new \ArrayIterator($this->hashFields);
+     }
+
+     /**
+      * (PHP 5 &gt;= 5.0.0)<br/>
+      * Whether a offset exists
+      * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+      * @param mixed $offset <p>
+      * An offset to check for.
+      * </p>
+      * @return boolean true on success or false on failure.
+      * </p>
+      * <p>
+      * The return value will be casted to boolean if non-boolean was returned.
+      */
+     public function offsetExists($offset)
+     {
+         return array_key_exists($offset, $this->hashFields);
+     }
+
+     /**
+      * (PHP 5 &gt;= 5.0.0)<br/>
+      * Offset to retrieve
+      * @link http://php.net/manual/en/arrayaccess.offsetget.php
+      * @param mixed $offset <p>
+      * The offset to retrieve.
+      * </p>
+      * @return mixed Can return all value types.
+      */
+     public function offsetGet($offset)
+     {
+         return $this->offsetExists($offset) ? $this->hashFields[$offset] : null;
+     }
+
+     /**
+      * (PHP 5 &gt;= 5.0.0)<br/>
+      * Offset to set
+      * @link http://php.net/manual/en/arrayaccess.offsetset.php
+      * @param mixed $offset <p>
+      * The offset to assign the value to.
+      * </p>
+      * @param mixed $value <p>
+      * The value to set.
+      * </p>
+      * @return void
+      */
+     public function offsetSet($offset, $value)
+     {
+         if (!is_null($offset)) {
+             $this->hashFields[$offset] = $value;
+         }
+     }
+
+     /**
+      * (PHP 5 &gt;= 5.0.0)<br/>
+      * Offset to unset
+      * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+      * @param mixed $offset <p>
+      * The offset to unset.
+      * </p>
+      * @return void
+      */
+     public function offsetUnset($offset)
+     {
+         if ($this->offsetExists($offset)) {
+             unset($this->hashFields[$offset]);
+         }
+     }
+ }
