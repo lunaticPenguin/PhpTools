8a155a353962dbb8e0508fbbed428ebb40126c15
Corentin Legros <corentin.legros@gmail.com>
2015-09-29 03:37:54 +0200

    Better behaviour for entities

--- app/entities/AbstractEntity.php
+++ app/entities/AbstractEntity.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Entities;
+use App\Exceptions\EntityException;
 
 /**
  * Class AbstractEntity.
@@ -12,27 +13,52 @@ abstract class AbstractEntity
     const PROCESS_CREATE = 0;
     const PROCESS_UPDATE = 1;
 
+    protected $intId = 0;
+
+    /**
+     * Construct a new entity that used models layer
+     *
+     * @param integer $intId
+     * @throws EntityException
+     */
+    public function __construct($intId = 0)
+    {
+        $intId = (integer) $intId;
+        if ($intId < 0) {
+            throw new EntityException(sprintf('Wrong entity id given (%d)', $intId));
+        }
+        $this->intId = $intId;
+    }
+
+    /**
+     * Allows to save changes for the current entity
+     * @param $hashData
+     * @param int $intType
+     * @return boolean
+     */
+    protected abstract function processSave($hashData, $intType = self::PROCESS_CREATE);
+
     /**
      * Allows to check data coherence during a process save.
      * A type of save can be provided.
      * @param array $hashData
      * @param integer $intType
-     * @return bool
+     * @return boolean
      */
-    protected static function checkSave($hashData, $intType = self::PROCESS_CREATE)
-    {
-
-    }
+    protected abstract function checkSave($hashData, $intType = self::PROCESS_CREATE);
 
     /**
      * Process save operation for the current entity.
-     * This method should call self::checkSave() method
+     *
      * @param array $hashData
      * @param integer $intType
-     * @return mixed
+     * @return boolean
      */
-    protected static function processSave($hashData, $intType = self::PROCESS_CREATE)
+    final public function save($hashData, $intType = self::PROCESS_CREATE)
     {
-
+        if (!$this->checkSave($hashData, $intType)) {
+            return false;
+        }
+        return $this->processSave($hashData);
     }
 }
---
+++ app/exceptions/EntityException.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Exceptions;
+
+class EntityException extends \Exception
+{
+
+}
